08f918b39ec8d52e378005af192934f310b9689c
BangNA1 <nguyenbanghoi1997@gmail.com>
2022-11-05 02:11:11 +0700

    my commit

---
+++ src/index.js
@@ -0,0 +1,39 @@
+import express from 'express';
+import morgan from 'morgan';
+import { create } from 'express-handlebars';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import path from 'path';
+import { route } from './routes/index.js';
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const hbs = create({
+    extname: '.hbs',
+});
+
+app.use(
+    express.urlencoded({
+        extended: true,
+    }),
+);
+app.use(express.json());
+
+// HTTP logger
+// app.use(morgan('combined'))
+
+//Template engine
+app.engine('hbs', hbs.engine);
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, './resource/views'));
+app.use(express.static(path.join(__dirname, '/public')));
+
+// Routes init
+route(app);
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
